d0632555eea1357c20e0b968fbc3a64397973481
losethewar <furkansimek@gmail.com>
2021-05-11 01:36:15 +0300

    Initial commit

---
+++ js/clock.js
@@ -0,0 +1,48 @@
+function askName() {
+    var name = prompt("Adınız nedir?");
+    document.getElementById("myName").innerText = name;
+}
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = date.getDay();
+
+    switch (session) {
+        case 1:
+            day = "Pazartesi";
+            break;
+        case 2:
+            day = "Salı";
+            break;
+        case 3:
+            day = "Çarşamba";
+            break;
+        case 4:
+            day = "Perşembe";
+            break;
+        case 5:
+            day = "Cuma";
+            break;
+        case 6:
+            day = "Cumartesi";
+            break;
+        case 7:
+            day = "Pazar";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + day;
+    document.getElementById("myClock").innerText = time;
+    document.getElementById("myClock").textContent = time;
+
+    setTimeout(showTime, );
+}
+
+askName();
+showTime();
